fa03c34aa95dae56668ba0ec22c3690be89ba16b
yvanbourdin <yvankiller92@gmail.com>
2019-04-16 14:50:44 +0200

    Mon app Django Girls, premier commit

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post
+
+# Nom d'utilisateur : ola
+# Adresse mail : ola@example.com
+# Mot de passe : adminadmin
+
+
+admin.site.register(Post)
+
